d99aed2a70c60fa39fda31713ad1e66063e20525
Sergey Manukyan <smanukyan@rgare.com>
2016-05-23 16:29:26 +0100

    Separated log files for each method

--- config-dir/run.sh
+++ config-dir/run.sh
@@ -1,18 +1,36 @@
 #!/usr/bin/env bash
 
-# This script needs to run in config-dir only.
-echo Directory : ${PWD}
+# Usage :
+# ./run.sh stdin-to-stdout
+# ./run.sh file-to-stdout
+# ./run.sh file-to-file
 
-# Delete old log file if exists.
-LOG_FILE=logstash.log
-echo Log file : ${LOG_FILE} - `[ -f ${LOG_FILE} ] && { echo -n " FOUND... " ; rm ${LOG_FILE} && echo Deleted. ; } || echo NOT FOUND.`
+# This script needs to be run from config-dir folder only.
+echo -n "Current directory : ${PWD} - "
+if [ "${PWD##*/}" = "config-dir" ]
+then
+    echo OK.
+else
+    echo FAIL.
+    echo Must run this script from config-dir folder. ABORTING!
+    exit 1
+fi
 
 # Check configuration file.
 CONF_NAME=${1}
 CONF_FILE=logstash-${CONF_NAME}.conf
-echo Configuration file : ${CONF_FILE} - `[ -f ${CONF_FILE} ] && echo FOUND || { echo NOT FOUND. ABORTING! ; exit 1 ; }`
+echo -n "Configuration file : ${CONF_FILE} - "
+if [ -f ${CONF_FILE} ]
+then
+    echo FOUND.
+else
+    echo NOT FOUND. ABORTING!
+    exit 1
+fi
 
-touch output.log
+# Delete old log file if exists.
+LOG_FILE=logstash-${CONF_NAME}.log
+echo Log file : ${LOG_FILE} - `[ -f ${LOG_FILE} ] && { echo -n " FOUND... " ; rm ${LOG_FILE} && echo Deleted. ; } || echo NOT FOUND.`
 
 echo Starting docker...
 docker run \
